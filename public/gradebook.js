18c2f49c11b339ee91c933ade2a3792ab6dbd352
k8thegrr8 <108436044+k8thegrr8@users.noreply.github.com>
2025-07-20 16:07:14 -0400

    Changes

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,10 +1,3 @@
-// TODO: Populate the table with grade data
-function populateGradebook(data) {
-    // This function will take the fetched grade data and populate the table 
-    console.log("Populating gradebook with data:", data);
-}
-
-
 function fetchGradeData() {
     //This function will query the PostgreSQL database and return grade data 
     console.log("Fetching grade data...");
