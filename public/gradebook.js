5ffbe96829cbaeea085a9e8cd0c382f8f358f59f
k8thegrr8 <108436044+k8thegrr8@users.noreply.github.com>
2025-07-20 16:14:23 -0400

    Changed

--- public/gradebook.js
+++ public/gradebook.js
@@ -14,6 +14,7 @@ function fetchGradeData() {
             if(xhr.status !== 200){
                 console.error(`Could not get grades. 
                     Status: ${xhr.status}`);
+                    return;
             }
             // And then call the function to update the HTML with our data
             populateGradebook(JSON.parse(xhr.responseText));
@@ -49,3 +50,6 @@ function populateGradebook(data) {
             tableElm.appendChild(row);
         });
 }
+window.onload = function () {
+    fetchGradeData();
+};
